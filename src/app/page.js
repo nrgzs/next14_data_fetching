31c0a8e62c1b85773850de4500cdb17593b70cf0
nergız seyidova <nergisseyidova@gmail.com>
2024-03-14 14:28:53 +0400

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,157 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.js</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>Explore starter templates for Next.js.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
+  );
+}
+
+
+
+/* import { useState } from "react";
+import ky from "ky";
+
+export const getStaticProps = async () => {
+  try {
+    const data = await ky("https://jsonplaceholder.typicode.com/posts").json();
+    return {
+      props: { posts: data },
+    };
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return {
+      notFound: true,
+    };
+  }
+};
+const Home = ({ posts }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const postsPerPage = 1;
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const decrementPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  const incrementPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  return (
+    <main>
+      <div>
+        <button onClick={decrementPage} disabled={currentPage === 1}>
+          Prev
+        </button>
+        <div>
+          <ul>
+            {currentPosts.map(({ id, title }) => (
+              <li key={id}>{title}</li>
+            ))}
+          </ul>
+        </div>
+        <button
+          onClick={incrementPage}
+          disabled={indexOfLastPost >= posts.length}
+        >
+          Next
+        </button>
+      </div>
+    </main>
+  );
+};
+
+export default Home; */
